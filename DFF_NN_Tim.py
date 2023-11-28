934f05919a4f28c1fda2a9d73daae04f9a4e9d3c
TimGeissler <tim.geissler@study.hs-duesseldorf.de>
2023-11-28 22:44:19 +0100

    - plot data, model not trained well yet

--- DFF_NN_Tim.py
+++ DFF_NN_Tim.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
+from tensorflow import keras
+from keras.optimizers import Adam
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+
+# Training auf GPU verlagern
+#physical_devices = tf.config.experimental.list_physical_devices
+#print(physical_devices)
 
 RELPATH = 'D:\\Tim\\Programmierung\\Playground\\Daten\\'
 
@@ -25,7 +32,7 @@ for datei in os.listdir(RELPATH):
                 dataTE[indexTE] = dataTE[indexTE][:, :480]
 
                 size = (len(dataTE[indexTE]), len(dataTE[indexTE][0]))
-                print(f'Größe der Matrix aus {datei}: {size}')
+                #print(f'Größe der Matrix aus {datei}: {size}')
                 indexTE += 1
     else:
         with open(filePath, 'r') as file:
@@ -35,20 +42,11 @@ for datei in os.listdir(RELPATH):
                 size = (len(data[index]), len(data[index][0]))
                 if (size[0] > 52):
                       data[index] = np.transpose(data[index])
-                print(f'Größe der Matrix aus {datei}: {size}')
+                #print(f'Größe der Matrix aus {datei}: {size}')
                 index += 1
-
-#Debug
-#print(index)
-#print(indexTE)
-#
-
-
 
 # für jeden Fehler (alle 22 Dateien) trainieren
 # 
-
-
 
 # Erstellen des Modells bzw. Neuronalen Netzes 
 
@@ -62,20 +60,31 @@ model.add(tf.keras.layers.Dense(52, activation = tf.nn.relu)) # Vorlesung
 # zweite Schicht des NN mit 128 Neuronen und der Aktivierungsfunktion (z.B Sprungfunktion, Sigmoid-Funktion (Schwanenhals-Funktion))
 model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu))
 # Ausgangsschicht des NN mit 21 Neuronen, da es 21 Fälle gibt. ie Aktivierungsfunktion ist hier eine Wahrscheinlichkeitsverteilung
-model.add(tf.keras.layers.Dense(21,  activation = tf.nn.softmax)) #stimmt so noch nicht mit 21 Fällen 
+model.add(tf.keras.layers.Dense(22,  activation = tf.nn.softmax)) #stimmt so noch nicht mit 21 Fällen 
+
+
 
 model.compile(optimizer='adam',
              loss='sparse_categorical_crossentropy',
              metrics=['accuracy'])
 
-for faulttype in data:
-      x_train = np.array(faulttype)
-      #print(x_train.shape)
-      #x_train_vector = np.array(x_train[:,0])
-      #print(x_train_vector.shape)
-      y_train = np.random.randint(21, size=(x_train.shape[0]))
 
-      model.fit(x_train, y_train, epochs=100) # Hier scheint es ein problem damit zu geben das lable0 
 
+#print(np.eye(22))
+#x_train = np.array(data / np.max(data))
+#y_train = np.arange(22) / np.max(np.arange(22))
+#y_train = np.arange(22) / np.max(np.arange(22))
+
+
+for i in range(22):
+      #falls geplottet werden soll
+      #x = np.arange(480)
+      #y = np.vstack(np.transpose(file))
+      #fig, ax = plt.subplots()
+      #ax.plot(x, y)
+      #plt.show()
+      x_train = np.array(data[i])
+      y_train = np.full(52, i)
+      model.fit(x_train, y_train, epochs=3) # Hier scheint es ein problem damit zu geben das lable0
 
 print('done without error lol how come')
