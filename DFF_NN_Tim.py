4d24d19d92fc5b7f8f09cb074e691c54e09ee07c
TimGeissler <tim.geissler@study.hs-duesseldorf.de>
2023-11-29 20:55:07 +0100

    - model trains okay (?), prediction shitty

--- DFF_NN_Tim.py
+++ DFF_NN_Tim.py
@@ -45,9 +45,6 @@ for datei in os.listdir(RELPATH):
                 #print(f'Größe der Matrix aus {datei}: {size}')
                 index += 1
 
-# für jeden Fehler (alle 22 Dateien) trainieren
-# 
-
 # Erstellen des Modells bzw. Neuronalen Netzes 
 
 # Sequential Model -> Feed-Forward Model 
@@ -56,35 +53,38 @@ model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten()) # noch keine Ahnung was das bedeutet (Nacharbeiten)
 
 # erste Schicht des NN mit 52 Neuronen und der Aktivierungsfunktion (z.B Sprungfunktion, Sigmoid-Funktion (Schwanenhals-Funktion))
-model.add(tf.keras.layers.Dense(52, activation = tf.nn.relu)) # Vorlesung
+model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu)) # Vorlesung
 # zweite Schicht des NN mit 128 Neuronen und der Aktivierungsfunktion (z.B Sprungfunktion, Sigmoid-Funktion (Schwanenhals-Funktion))
 model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu))
-# Ausgangsschicht des NN mit 21 Neuronen, da es 21 Fälle gibt. ie Aktivierungsfunktion ist hier eine Wahrscheinlichkeitsverteilung
-model.add(tf.keras.layers.Dense(22,  activation = tf.nn.softmax)) #stimmt so noch nicht mit 21 Fällen 
-
-
+# Ausgangsschicht des NN mit 22 Neuronen, da es 21 Fälle gibt. ie Aktivierungsfunktion ist hier eine Wahrscheinlichkeitsverteilung
+model.add(tf.keras.layers.Dense(22,  activation = tf.nn.softmax)) #stimmt so noch nicht mit 22 Fällen 
 
 model.compile(optimizer='adam',
              loss='sparse_categorical_crossentropy',
              metrics=['accuracy'])
 
+#predictions = []
 
-
-#print(np.eye(22))
-#x_train = np.array(data / np.max(data))
-#y_train = np.arange(22) / np.max(np.arange(22))
-#y_train = np.arange(22) / np.max(np.arange(22))
-
-
-for i in range(22):
+#for i in range(22):
       #falls geplottet werden soll
       #x = np.arange(480)
-      #y = np.vstack(np.transpose(file))
+      #y = np.vstack(np.transpose(data[i]))
       #fig, ax = plt.subplots()
       #ax.plot(x, y)
       #plt.show()
-      x_train = np.array(data[i])
-      y_train = np.full(52, i)
-      model.fit(x_train, y_train, epochs=3) # Hier scheint es ein problem damit zu geben das lable0
+      #x_train = np.array(data[i] / np.max(data[i]))
+      #print(x_train)
+      #y_train = np.full(52, i)
+      #model.fit(x_train, y_train, epochs=3)
+      #print(model.predict(dataTE[i]))
 
+x_train = np.array(data / np.max(data))
+print(x_train.shape)
+y_train = np.arange(22)
+model.fit(x_train, y_train, epochs=1000) # läuft einigermaßen
+
+#print(dataTE[1].shape)
+#print(model.predict(dataTE[1]))
+
+#print(predictions)
 print('done without error lol how come')
