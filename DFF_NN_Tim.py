0b9da00cfa2683ac0095b5dccb7e9bbfd05779a7
TimGeissler <tim.geissler@study.hs-duesseldorf.de>
2023-12-08 08:17:02 +0100

    - labels after talking to oli still not good

--- DFF_NN_Tim.py
+++ DFF_NN_Tim.py
@@ -54,40 +54,43 @@ model = tf.keras.models.Sequential()
 #model.add(tf.keras.layers.Flatten()) # noch keine Ahnung was das bedeutet (Nacharbeiten)
 
 # erste Schicht des NN mit 52 Neuronen und der Aktivierungsfunktion (z.B Sprungfunktion, Sigmoid-Funktion (Schwanenhals-Funktion))
-model.add(tf.keras.layers.Dense(64, activation = tf.nn.relu)) # Vorlesung
+model.add(tf.keras.layers.Dense(52, activation = tf.nn.relu)) # Vorlesung
 # zweite Schicht des NN mit 128 Neuronen und der Aktivierungsfunktion (z.B Sprungfunktion, Sigmoid-Funktion (Schwanenhals-Funktion))
-model.add(tf.keras.layers.Dense(256, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu))
 # Ausgangsschicht des NN mit 22 Neuronen, da es 21 Fälle gibt. ie Aktivierungsfunktion ist hier eine Wahrscheinlichkeitsverteilung
-model.add(tf.keras.layers.Dense(22,  activation = tf.nn.relu)) #stimmt so noch nicht mit 22 Fällen 
+#model.add(tf.keras.layers.Dense(22,  activation = tf.nn.relu)) #stimmt so noch nicht mit 22 Fällen 
 model.add(tf.keras.layers.Dense(22, activation = tf.nn.softmax))
 
 model.compile(optimizer='adam',
              loss='sparse_categorical_crossentropy',
              metrics=['accuracy'])
 
-#predictions = []
+numberOfEpochs = 1
 
-for m in range(10):
-      for i in range(22):
-            #falls geplottet werden soll
+#falls geplottet werden soll
             #x = np.arange(480)
             #y = np.vstack(np.transpose(data[i]))
             #fig, ax = plt.subplots()
             #ax.plot(x, y)
             #plt.show()
-            x_train = np.transpose(data[i-1] / np.max(data[i-1]))
+
+for m in range(480):
+      for i in range(22):
+            x_train = np.transpose(data[i] / np.max(data))
+            x_train = np.array(x_train[m])
             #print(x_train.shape)
-            y_train = np.full(480, i)
-            #y_train = np.array(1)
-            numberOfEpochs = 10
+            y_train = np.eye(22)[i, :]
+            #y_train = np.array(y_train)
+            #print(y_train)
+            #y_train = np.array(1)            
             model.fit(x_train, y_train, epochs=numberOfEpochs)
 
 prediction = [] 
-for n in range(22):
-      print('now predicting: ' + str(n))
-      for o in range(480):
-            prediction.append(model.predict(np.transpose(dataTE[n])[m]))
-            print(np.argmax(model.predict(np.transpose(dataTE[n])[m])))
+#for n in range(22):
+#      print('now predicting: ' + str(n))
+#      for o in range(480):
+#            prediction.append(model.predict(np.transpose(dataTE[n])[m]))
+#            print(np.argmax(model.predict(np.transpose(dataTE[n])[m])))
 
 
 #x_train = np.array(data / np.max(data))
